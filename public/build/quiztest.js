8c04a25a6dda3f315917a71b254e3665444e1462
nitin babariya <nitin.babariya@betssongroup.com>
2023-03-05 21:49:43 +0100

    fix(.github-workflow-build.yaml): Updating the deploy branch to main

--- public/build/quiztest.js
+++ public/build/quiztest.js
@@ -1,4 +1,4 @@
-/* Version: 0.1.0 - March 4, 2023 00:50:45 */
+/* Version: 0.1.0 - March 5, 2023 16:13:56 */
 
 (function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 (function (global, factory) {
